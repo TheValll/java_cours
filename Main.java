23108c4a183b7a5d5200389d1bce69ffc59e3426
Valentin Massonnière <vmassonniere@adloox.com>
2024-09-25 14:29:43 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,61 @@
+public class Main {
+  public static void main(String[] args) {
+    // println utility
+    System.out.println("Hello World");
+    System.out.println("I am learning Java.");
+    System.out.println("It is awesome!");
+
+    // print utility
+    System.out.print("Hello World! ");
+    System.out.print("I will print on the same line.");
+
+    // println numbers
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(50000);
+
+    // println maths
+    System.out.println(3 + 3);
+    System.out.println(2 * 5);
+
+    // Variables
+    String name = "Valentin";
+    System.out.println(name);
+
+    int a = 100;
+    int b = 30;
+
+    System.out.println(a + b);
+
+    int c = 10;
+    c = 100;
+    System.out.println(c);
+
+    final int d = 8432;
+    System.out.println(d);
+
+    // int myNum = 5;
+    // float myFloatNum = 5.99f;
+    // char myLetter = 'D';
+    // boolean myBool = true;
+    // String myText = "Hello";
+
+    // Declare many variables
+    int x = 5, y = 6, z = 50;
+    System.out.println(x + y + z);
+
+    // Student data
+    String studentName = "John Doe";
+    int studentID = 15;
+    int studentAge = 23;
+    float studentFee = 75.25f;
+    char studentGrade = 'B';
+
+    // Print variables
+    System.out.println("Student name: " + studentName);
+    System.out.println("Student id: " + studentID);
+    System.out.println("Student age: " + studentAge);
+    System.out.println("Student fee: " + studentFee);
+    System.out.println("Student grade: " + studentGrade);
+  }
+}
